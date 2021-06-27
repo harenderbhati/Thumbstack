4ead3bd334ad7ed4aca0943bb7ad3ba5d66c5f99
Harender bhati <harrythakur1101@gmail.com>
2021-06-27 22:31:13 +0530

    Making Thumbstack project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {AppRoutingModule} from './app-routing/app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {  FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    // FormControl,
+    // FormBuilder,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatInputModule
+
+  
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,49 @@
+import { getQueryPredicate } from '@angular/compiler/src/render3/view/util';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms'
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+
+   status:false;
+
+    itemsList=[
+      {name:"Pizza",price:120, status:false},
+      {name:"Cheese",price:180, status:false},
+      {name:"Mushroom",price:240, status:false},
+      {name:"Mushroom",price:240, status:false},
+      {name:"Mushroom",price:240, status:false},
+  ];
+   
+    subtotal=0;
+    tip=0;
+
+    total=0;
+
+   
+  
+
+ ngOnInit(): void {
+   
+ 
+}
+
+getPrice(price,status){
+    if (status==false){
+      this.subtotal+= price;
+    }else{
+      this.subtotal-=price;
+    }
+ 
+  console.log(status) ;
+}
+
+tipPer(tip){
+     this.total = (tip*this.subtotal)/100  + this.subtotal; 
+}
+
+
+}
